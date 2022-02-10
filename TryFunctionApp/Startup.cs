3d1c7d992c555b420a6006c463d8a9580c03e7cf
jithesh <jitheshc@hp.com>
2022-02-09 20:58:32 -0800

    adding fun app

---
+++ TryFunctionApp/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+
+[assembly: FunctionsStartup(typeof(TryFunctionApp.Startup))]
+namespace TryFunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        string VERSION = "1.0.0";
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var userAssignedClientId = "7d53794d-edb9-4a1f-b8f6-41f91c2f02ba";
+
+            Console.WriteLine($"!!!!!!!!!!!!--StartUP Version = {VERSION} !!!!!!!!!!!!-----------------------");
+            builder.Services.AddLogging();
+            builder.Services.AddAzureClients(clientBuilder =>
+            {
+
+                clientBuilder.AddBlobServiceClient(new Uri("https://try01storage.blob.core.windows.net"))
+                                .WithCredential(new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userAssignedClientId }));
+                clientBuilder.AddQueueServiceClient(new Uri("https://try01storage.queue.core.windows.net"))
+                              .WithCredential(new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userAssignedClientId }))
+                             .ConfigureOptions(c => c.MessageEncoding = Azure.Storage.Queues.QueueMessageEncoding.Base64);
+            });
+            try
+            {
+                var cosmosClient = new CosmosClient("https://try01cosmos.documents.azure.com:443/", new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userAssignedClientId }));
+                if (cosmosClient == null)
+                {
+                    Console.WriteLine("#### Cosmos Client Failed to Create");
+                }
+                builder.Services.AddSingleton(cosmosClient);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception !!!!");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("******** StartUP Ended ****************");
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+
+            //FunctionsHostBuilderContext context = builder.GetContext();
+
+
+
+            //builder.ConfigurationBuilder
+            //    .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), true, false)
+            //    .AddEnvironmentVariables()
+            //    //.AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+            //    .Build();
+
+            //            builder.Configuration.AddJsonFile("appsettings.json");
+            //#if DEBUG
+            //            builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly());
+            //            ConfigureMSIEnvironment(builder);
+            //#endif
+            //            builder.Configuration.AddEnvironmentVariables();
+            //            builder.Configuration.AddAzureAppConfiguration(opions =>
+            //            {
+            //                opions.Connect(new Uri("https://hpsmart-config.azconfig.io"), new DefaultAzureCredential());
+            //            });
+
+            //            builder.Configuration.AddAzureKeyVault(
+            //                   new Uri($"https://smartexkeyvault.vault.azure.net/"),
+            //                   new DefaultAzureCredential());
+        }
+
+
+    }
+}
---
+++ TryFunctionApp/TryFun.cs
@@ -0,0 +1,78 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Queues;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TryFunctionApp
+{
+    public class TryFun
+    {
+        private readonly BlobServiceClient blbClient;
+        private readonly QueueServiceClient qClient;
+        private readonly CosmosClient cosmosClient;
+
+        public TryFun(BlobServiceClient blbClient, QueueServiceClient qClient, CosmosClient cosmosClient)
+        {
+            this.blbClient = blbClient;
+            this.qClient = qClient;
+            this.cosmosClient = cosmosClient;
+        }
+
+        [FunctionName("TryFun")]
+        public async Task Run([TimerTrigger("*/1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var containerClient = blbClient.GetBlobContainerClient("logs");
+            BlobClient blobClient = containerClient.GetBlobClient(Guid.NewGuid().ToString());
+
+            using (var ms = new MemoryStream())
+            {
+                StreamWriter writer = new StreamWriter(ms);
+                writer.Write(DateTime.Now.ToString());
+                writer.Flush();
+                ms.Position = 0;
+                await blobClient.UploadAsync(ms);
+            }
+
+            int count = 0;
+
+            await foreach (BlobItem item in containerClient.GetBlobsAsync())
+            {
+                count++;
+                Console.WriteLine(item.Name);
+            }
+
+            var sq = qClient.GetQueueClient("sessions");
+            await sq.SendMessageAsync("a new one " + DateTime.Now.ToString());
+
+            var container = cosmosClient.GetContainer("tryDb", "tries");
+            var payload = new CosmosModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Pk = "123",
+                Name = "Jithesh",
+                Time = DateTime.Now.ToShortDateString()
+            };
+            await container.CreateItemAsync(payload);
+        }
+    }
+
+    class CosmosModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "pk")]
+        public string Pk { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+}
